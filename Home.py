b29edbb9c6508b1a34cc19d144565230f3b64865
Kandarp Dubey <kandarp1415@gmail.com>
2024-09-19 15:53:06 +0530

    abc

--- Home.py
+++ Home.py
@@ -7,7 +7,6 @@ root=Tk()
 root.title("PlantHealthAI")
 root.geometry("1910x1070")
 root.config(bg="#37B7C3")
-
 frame=Frame(root,width=1200, height=550, bg="#E7F0DC")
 frame.place(relx=0.5,rely=0.5, anchor="center")
 
