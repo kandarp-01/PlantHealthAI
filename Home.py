4facd8e9a4159c013e3e9ab2be8b297a7de14702
Aman-patel-18 <amanp7381@gmail.com>
2024-09-19 15:15:00 +0530

    Update

--- Home.py
+++ Home.py
@@ -1,5 +1,7 @@
 from tkinter import *
+from PIL import Image
 import os
+import matplotlib
 
 root=Tk()
 root.title("PlantHealthAI")
@@ -9,10 +11,7 @@ root.config(bg="#37B7C3")
 frame=Frame(root,width=1200, height=550, bg="#E7F0DC")
 frame.place(relx=0.5,rely=0.5, anchor="center")
 
-Label(text="Welcome to PlantHealth AI ......",font=('arial',40,'bold')).place(x=377, y=170)
-
-
-
+Label(text="Welcome to $6000 AI ......",font=('arial',40,'bold')).place(x=377, y=170)
 def open():
     root.destroy()
     os.system("software\\2nd_page.py")
