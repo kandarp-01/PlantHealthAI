da30af0b1fd445942b1c3587a6ae7150d5c4f567
Kandarp Dubey <kandarp1415@gmail.com>
2024-09-23 16:43:42 +0530

    Plant Data

--- result.py
+++ result.py
@@ -1,9 +1,14 @@
 from tkinter import *
-root=Tk()
-root.geometry(("1400x750"))
 def choose():
     pass
-b1=Button(text="View Your Result",command=choose,width=15,height=4)
+root=Tk()
+root.geometry(("1400x800"))
+frame1=Frame(root,width=1200,height=600)
+frame1.config(bg="white")
+l1=Label(frame1,text="View Your Result",bg="white",font=("Arial",40),fg="black")
+b1=Button(text="Show Result",command=choose,width=15,height=4)
+frame1.place(relx=0.5,rely=0.5,anchor=CENTER)
 b1.place(x=700,y=500)
-root.config(bg="black")
+l1.place(x=400,y=40)
+root.config(bg="#16423C")
 root.mainloop()
