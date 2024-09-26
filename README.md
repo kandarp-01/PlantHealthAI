87fa299f3f3c46e0fb6460687315c60419bb085e
Kandarp Dubey <kandarp1415@gmail.com>
2024-09-26 21:42:51 +0530

    Data

--- README.md
+++
@@ -1,2 +0,0 @@
-# PlantHealthAI
-It tell about the condition of plant and provide the remedies by scan image of it.
--- pick.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-root=Tk()
-def choose():
-    pass
-b1=Button(text="Upload from PC",command=choose)
-root.mainloop()
