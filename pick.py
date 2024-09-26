6f679e00f736a9c086e902dcb0967380b4fac50a
Kandarp Dubey <kandarp1415@gmail.com>
2024-09-26 22:19:32 +0530

    changes

--- pick.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-root=Tk()
-root.minsize(height=300,width=400)
-def choose():
-    pass
-b1=Button(text="Upload from PC",command=choose,width=15,height=4)
-b1.place(x=700,y=500)
-root.config(bg="black")
-root.mainloop()
